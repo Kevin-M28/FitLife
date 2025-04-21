959cccd8850088b6acdba6b990aea398f2ef2a64
MónicaSC <monica.sanchez.chaves@est.una.ac.cr>
2025-04-21 08:46:12 -0600

    Entidad
    SesionUsuario

---
+++ BackEnd/Entidades/SesionUsuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.Entidades
+{
+    public class SesionUsuario
+    {
+        public string Id { get; set; }
+        public int idGym { get; set; }
+        public string idUsuario { get; set; }
+        public string token { get; set; }
+        public DateTime fechaCreacion { get; set; }
+        public DateTime fechaExpiracion { get; set; }
+        public string direccionIP { get; set; }
+        public bool activo { get; set; }
+        public DateTime ultimoAcceso { get; set; }
+    }
+}
